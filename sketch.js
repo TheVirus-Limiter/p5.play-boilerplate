678fb9fd5bd7461ba60658717923d29a450661cf
TheVirus-Limiter <kleensweep.pypx@gmail.com>
2020-10-05 16:56:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,34 @@
+var fixedRect, movingRect;
+
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1200,800);
+movingRect = createSprite(400, 200, 80, 30);
+ fixedRect = createSprite(200,200,60,80);
+ fixedRect.shapeColor = "green";
+ movingRect.shapeColor = "green";
+ movingRect.debug = true;
+ fixedRect.debug = true;
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0);
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  if (movingRect.x - fixedRect.x <= fixedRect.width/2 + movingRect.width/2 && 
+     fixedRect.x - movingRect.x <= movingRect.width/2 + fixedRect.width/2 && movingRect.y - fixedRect.y <= fixedRect.height/2 + movingRect.height/2 && 
+     fixedRect.y - movingRect.y <= movingRect.height/2 + fixedRect.height/2 ){                                                                  
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  else {
+    fixedRect.shapeColor = "green";
+    movingRect.shapeColor = "green";
+
+    
+
+
+  }
+
   drawSprites();
 }
